baba8d4d9ace77774ff25fa410caf1ecf3c7dc26
Yashesh Pratap Singh <yashesh.pratap.ug21@nsut.ac.in>
2024-07-09 19:08:16 +0530

    created a todo app using modern GUI

---
+++ modernTODOapp.py
@@ -0,0 +1,21 @@
+import customtkinter as ctk
+
+def add_todo():
+    todo=entry.get()
+    label=ctk.CTkLabel(scrollable_frame,text=todo)
+    label.pack()
+    entry.delete(0,ctk.END)
+root=ctk.CTk()
+root.geometry("750x450")
+root.title("TODO App")
+title_label=ctk.CTkLabel(root,text="Daily Tasks",font=ctk.CTkFont(size=30,weight="bold"))
+title_label.pack(padx=10,pady=(40,20))
+
+scrollable_frame=ctk.CTkScrollableFrame(root,width=500,height=200)
+scrollable_frame.pack()
+
+entry=ctk.CTkEntry(scrollable_frame,placeholder_text="Add todo")
+entry.pack(fill="x")
+add_btn=ctk.CTkButton(root,text="Add",width=500,command=add_todo)
+add_btn.pack()
+root.mainloop()
